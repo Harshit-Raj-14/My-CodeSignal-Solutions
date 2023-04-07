4787497822334d026afc4c6c99d6d6d03e035a45
Harshit Raj <harshit.raj2021@vitbhopal.ac.in>
2023-04-07 23:16:40 +0530

    Add files via upload

---
+++ Google Interview Questions/firstDuplicate.java
@@ -0,0 +1,8 @@
+int solution(int[] arr) {
+    HashSet<Integer> set = new HashSet<>();
+    for(int i=0;i<arr.length;i++){
+        if(set.contains(arr[i])) return arr[i];
+        set.add(arr[i]);
+    }
+    return -1;
+}
